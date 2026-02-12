0bf4da7779548f70cff93d9733b8412bfa0a69a7
ShmelZhopnyi <craftritch23@gmail.com>
2026-02-12 20:14:37 +0500

    Попов-Левин Алексей. Задание 01

--- task01/src/com/example/task01/Point.java
+++ task01/src/com/example/task01/Point.java
@@ -7,8 +7,33 @@ public class Point {
     int x;
     int y;
 
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+
     void print() {
         String pointToString = String.format("(%d, %d)", x, y);
         System.out.println(pointToString);
+    }
+
+    void flip(){
+
+        int temp = x;
+
+        x = -y;
+        y = -temp;
+    }
+
+    double distance(Point point){
+        double distanceX = this.x - point.x;
+        double distanceY = this.y - point.y;
+
+        return Math.sqrt(distanceX*distanceX + distanceY*distanceY);
+    }
+
+    public String toString(){
+        return "(" + x + "," + y +")";
     }
 }
